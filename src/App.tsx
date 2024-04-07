e41cec22dea83127215f5ca5c12ac9177552a4c4
Alexander Sowinski <125089619+alexsowinski@users.noreply.github.com>
2024-04-07 16:19:26 -0700

    header and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="header">
+            <h1>Alexander Sowinski</h1>
+        </div>
+        <div className="navigation">
+            <a href="https://github.com/alexsowinski">Projects!</a>
+            <a class="active" href="#home">About!</a>
+            <a href="https://www.linkedin.com/in/alexandersowinski/">LinkedIn!</a>
+        </div>
     </>
   )
 }
